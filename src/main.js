849fae5b1fd563cbfb974369e4a0efa6330d3298
aemaaeng <haemin120@naver.com>
2023-11-08 13:52:36 +0900

    design: reset.css 적용

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router.js";
+import "./assets/reset.css";
 
 const app = createApp(App).use(router);
 app.mount("#app");
